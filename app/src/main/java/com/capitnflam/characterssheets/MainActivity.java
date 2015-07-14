028881c1f450c4ef8006651ca576a743f939b51c
capitN.flam <capitnflam@gmail.com>
2015-07-14 02:38:25 +0200

    Adding games string-array resource

--- app/src/main/java/com/capitnflam/characterssheets/MainActivity.java
+++ app/src/main/java/com/capitnflam/characterssheets/MainActivity.java
@@ -1,17 +1,40 @@
 package com.capitnflam.characterssheets;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.res.Resources;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ResourceBundle;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
+
+    private static String[] Games = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Games = getResources().getStringArray(R.array.games);
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, Games);
+        final ListView listView = (ListView) findViewById(R.id.gamesListView);
+        listView.setAdapter(itemsAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id)
+            {
+                Toast.makeText(adapterView.getContext(), String.format("%s", Games[position]), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
