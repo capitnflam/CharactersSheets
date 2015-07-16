77013d671728b063a7161e51d166a3df0649db4e
capitN.flam <capitnflam@gmail.com>
2015-07-17 01:24:16 +0200

    Removing menu

--- app/src/main/java/com/capitnflam/characterssheets/MainActivity.java
+++ app/src/main/java/com/capitnflam/characterssheets/MainActivity.java
@@ -1,10 +1,8 @@
 package com.capitnflam.characterssheets;
 
-import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
@@ -35,27 +33,5 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(adapterView.getContext(), String.format("%s", Games[position]), Toast.LENGTH_SHORT).show();
             }
         });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
